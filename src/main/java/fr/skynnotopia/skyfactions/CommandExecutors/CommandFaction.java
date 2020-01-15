5bcc11168be1aa1f02bfebef301554753a035758
Deleranax <9z3KI8ininGZmy>
2020-01-15 18:30:44 +0100

    Ajout des TODOs Suppression de la commande /factions

--- src/main/java/fr/skynnotopia/skyfactions/CommandExecutors/CommandFaction.java
+++ src/main/java/fr/skynnotopia/skyfactions/CommandExecutors/CommandFaction.java
@@ -31,18 +31,81 @@ public class CommandFaction implements CommandExecutor {
 
         // Si le joueur n'a pas de faction
         if (sender.getFaction() == null) {
-            //TODO: Menu pour les joueurs sans faction
+            /*TODO: Menu pour les joueurs sans faction
+                Parcourir les factions
+                    Selectionner une faction (Créer un nouveau type de bouton) (liste de toutes les factions)
+                        Rejoindre
+                        Infos (...)
+                Créer une faction
+                    Ajouter le joueur dans la liste @see ChatInputListener
+            */
 
          // Si le joueur à une faction, mais n'est pas chef, ni officier
         } else if (!sender.isChief() && !sender.isOfficer()) {
-            //TODO: Menu pour les joueurs avec faction
+            /*TODO: Menu pour les joueurs avec faction
+                Parcourir les factions
+                        Infos (...)
+                Gestion des membres
+                    Liste (...)
+                Territoires
+                    Carte
+             */
+
 
         // Si le joueur à une faction, et est officier
         } else if (!sender.isChief()) {
-            //TODO: Menu pour les chef de faction
+            /*TODO: Menu pour les officiers de faction
+                Parcourir les factions
+                    Selectionner une faction (Créer un nouveau type de bouton) (liste de toutes les factions)
+                        Infos (...)
+                Membres
+                    Inviter
+                        Selectionner une membre (Créer un nouveau type de bouton) (liste de toutes les membres)
+                    Gestion des membres
+                        Selectionner une membre (Créer un nouveau type de bouton) (liste de toutes les membres)
+                            Promouvoir
+                            Expulser
+                Territoires
+                    Carte
+                    Revendiquer un chunk (position actuelle)
+                    Supprimer un chunk (position actuelle)
+                    Infos (...)
+            */
 
+            // Si le joueur à une faction, et est chef
         } else {
-
+            /*TODO: Menu pour les chef de faction
+                Parcourir les factions
+                    Selectionner une faction (Créer un nouveau type de bouton) (liste de toutes les factions)
+                        infos (...)
+                        Envoyer un message
+                        Déclarer la guerre / Rompre l'alliance
+                        Faire une alliance / Faire la paix
+                Membres
+                    Inviter
+                        Selectionner une membre (Créer un nouveau type de bouton) (liste de toutes les membres)
+                    Gestion des membres
+                        Selectionner une membre (Créer un nouveau type de bouton) (liste de toutes les membres)
+                            Promouvoir
+                            Promouvoir chef (vous remplace)
+                            Expulser
+                Territoires
+                    Carte
+                    Revendiquer un chunk (position actuelle)
+                    Supprimer un chunk (position actuelle)
+                    Infos (...)
+                 Diplomacie
+                    Relations
+                        Selectionner une faction (Créer un nouveau type de bouton) (liste de toutes les factions avec relation avec cette faction)
+                            Envoyer un message
+                            Rompre l'alliance/ Faire la paix
+                    Messages reçus
+                        Selectionner un message (Créer un nouveau type de bouton) (liste de tous les messages)
+                            Infos (message)
+                            Repondre
+                    Envoyer un message
+                        Selectionner une faction (Créer un nouveau type de bouton) (liste de toutes les factions)
+            */
         }
     return  true;
     }
